3a757cf6dd99a4aafa4f684dbc4f83c41d65ecf6
ankotyandayo <shuto.ando1@gmail.com>
2022-03-21 22:13:06 +0900

    2022/3/21

--- C - Unlucky 7.py
+++ C - Unlucky 7.py
@@ -1,4 +1,30 @@
+def judge_ten(x):
+  x = str(x)
+  if "7" in x:
+    return True
+  else:
+    return False
 
+
+def judge_eight(x):
+  s = ""
+  while x > 0:
+    s = str(x % 8) + s
+    x //= 8
+  if "7" in s:
+    return True
+  else:
+    return False
+
+
+N = int(input())
+ans = 0
+
+for i in range(1, N+1):
+  if judge_ten(i) == False and judge_eight(i) == False:
+    ans += 1
+
+print(ans)
 
 # 入力例
 # 20
