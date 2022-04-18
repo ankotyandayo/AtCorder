07112f902b9340ddd448bbcf7c8310b7c830495f
ankotyandayo <shuto.ando1@gmail.com>
2022-04-18 22:48:20 +0900

    2022/4/18

---
+++ C - Travel .py
@@ -0,0 +1,37 @@
+import itertools
+N, K = map(int, input().split())
+
+time = []
+
+for i in range(N):
+    T = list(map(int, input().split()))
+    time.append(T)
+
+ans = 0
+
+for root in itertools.permutations(range(1, N)):
+    now_time = 0
+    now_time += time[0][root[0]]
+    now_city = root[0]
+
+    for i in range(1, N-1):
+        to_city = root[i]
+        now_time += time[now_city][to_city]
+        now_city = to_city
+
+    now_time += time[now_city][0]
+    if now_time == K:
+        ans += 1
+
+print(ans)
+
+# https://atcoder.jp/contests/abc183/tasks/abc183_c
+# 入力例
+# 4 330
+# 0 1 10 100
+# 1 0 20 200
+# 10 20 0 300
+# 100 200 300 0
+
+# 出力例
+# 2
