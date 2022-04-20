99e38cc15d37be121a6e5a230e873622b81c4cdf
ankotyandayo <shuto.ando1@gmail.com>
2022-04-20 23:33:54 +0900

    2022/4/20

---
+++ C - Sum of product of pairs.py
@@ -0,0 +1,21 @@
+N = int(input())
+A = list(map(int, input().split()))
+mod = 10**9+7
+
+A_sum = sum(A)
+
+ans = 0
+
+for i in range(N):
+    A_sum -= A[i]
+    ans += A[i]*(A_sum)
+    ans %= mod
+
+print(ans)
+
+
+# 入力
+# 3
+# 1 2 3
+# 出力
+# 11
