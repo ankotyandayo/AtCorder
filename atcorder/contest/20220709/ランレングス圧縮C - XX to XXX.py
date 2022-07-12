f806ad441991a9d46a1b0ed40acb6356f5f560ef
安東秀斗 <shuto.ando1@gmail.com>
2022-07-12 22:48:53 +0900

    2022/7/12

--- atcorder/contest/20220709/ランレングス圧縮C - XX to XXX.py
+++ atcorder/contest/20220709/ランレングス圧縮C - XX to XXX.py
@@ -1,24 +1,41 @@
-from itertools import groupby
-ans == True
 
-def runLengthEncodeToString(S: str):
-    grouped = groupby(S)
-    res = []
-    for k, v in grouped:
-        res.append((k, int(len(list(v)))))
-    return res
-
-def main():
-    S = input()
-    T = input()
-    S_length = runLengthEncodeToString(S)
-    T_length = runLengthEncodeToString(T)
-    
-    for i in range(len(S_length)):
-        if S_length[i][1] != T_length[i][1]:
-            if S_length[i][1] < 2 or S_length[i][1] > T_length[i][1]:
-                ans == False
-                
-main()
-    
 # ランレングス圧縮
+
+from unittest import result
+
+
+S = input()
+T = input()
+
+def encode_runlength(s):
+    result = []
+    p = s[0]
+    a = 1
+    for c in s[1:]:
+        if p == c:
+            a += 1
+            continue
+        result.append((p,a))
+        p = c
+        a = 1
+    result.append((p,a))
+    return result
+            
+s = encode_runlength(S)
+t = encode_runlength(T)
+  
+if len(s) != len(t):
+    print('No')
+    exit()
+ 
+for (s0, s1), (t0, t1) in zip(s, t):
+    if s0 != t0:
+        print('No')
+        exit()
+    if s1 >= 2 and t1 > s1:
+        s1 = t1
+    if s1 != t1:
+        print('No')
+        exit()
+
+print('Yes')
