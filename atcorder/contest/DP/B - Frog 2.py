6a9666b8d32a007be619ae435bbc0f5fd9f6595f
安東秀斗 <shuto.ando1@gmail.com>
2022-09-26 23:46:31 +0900

    2022/9/26

--- atcorder/contest/DP/B - Frog 2.py
+++ atcorder/contest/DP/B - Frog 2.py
@@ -6,6 +6,8 @@ dp[1] = 0
 dp[2] = abs(h[1] - h[2])
 
 for i in range(3,N+1):
+    for k in range(i,k):
+        dp[i] = min(abs)
     dp[i] = min(abs(h[i] - h[i-1]) + dp[i-1],abs(h[i] - h[i-2]) + dp[i-2])
 print(dp[N])
     
