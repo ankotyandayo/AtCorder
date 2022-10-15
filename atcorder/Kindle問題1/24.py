52dcf3a31500f78f36ef67e7ad0e780cbe611d21
安東秀斗 <shuto.ando1@gmail.com>
2022-10-15 11:26:47 +0900

    24

--- atcorder/Kindle問題1/24.py
+++ atcorder/Kindle問題1/24.py
@@ -13,4 +13,23 @@ ans = 10 * 20
 for i in range(1<<N):
     cost = 0
     skill = [0] * M
+    for shift in range(N):
+        if i >> shift == 1:
+            cost += C[shift]
+            for j in range[M]:
+                skill[M] += A[shift][M]
+    if X <= min(skill):
+        ans = min(ans,cost)
+if ans == 10 * 20:
+    print('-1')
+else:
+    print(ans)
+            
+            
+
+# 3 3 10
+# 60 2 2 4
+# 70 8 7 9
+# 50 2 3 9
+
     
